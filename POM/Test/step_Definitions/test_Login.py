095d5d4e625fbd666a347266db22f05b8e629984
Gonzalo Molina <gonzalo.molina@darwoft.com>
2021-10-27 10:25:18 -0300

    incluye escenario out of stock y visual testing

--- POM/Test/step_Definitions/test_Login.py
+++ POM/Test/step_Definitions/test_Login.py
@@ -67,4 +67,3 @@ def step_verifyMyaccount(browser, message):
 
 if __name__ == '__main__':
      unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:\\Users\\admin\\PycharmProjects\\SegundoProyecto\\Reports'), verbosity=2)
-
